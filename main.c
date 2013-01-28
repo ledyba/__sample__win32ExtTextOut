a7e3b014795dc81b5aa3c452508e58edbb5c7bfd
psi <ledyba@users.sourceforge.jp>
2013-01-28 21:56:24 +0900

    コメント追加

--- main.c
+++ main.c
@@ -14,12 +14,14 @@ int main(int argc, char** argv)
 		return -1;
 	}
 	{
+		// cairoにおけるcairo_t的な存在。これに対して描画コマンドを発行する
 		HDC hdc = CreateCompatibleDC(NULL);
 		if( hdc == NULL ) {
 			fprintf(stderr, "Oops. failed to create compatible dc.");
 			return -1;
 		}
 		HBITMAP bmp;
+		// .bmpのヘッダを書かないとRGBの生データを流し込めない（驚愕）
 		BITMAPINFOHEADER ih;
 		{
 			memset(&ih, 0, sizeof(BITMAPINFOHEADER));
@@ -34,11 +36,18 @@ int main(int argc, char** argv)
 			ih.biYPelsPerMeter = 0;
 			ih.biClrUsed = 0;
 			ih.biClrImportant = 0;
+			// 前後のihの違いがよくわかんないんだけど、
+			// 前者は初期データのフォーマットを、
+			// 後者は作られるビットマップのフォーマットを
+			// それぞれ示してるみたい。で、一緒のフォーマット（RGB24）なので同じ内容になる。
+			// dataに初期データを入れてCBM_INITを指定するとそれが初期の絵になる。
+			// ここではmemsetで0xffで指定したので真っ白。
 			bmp = CreateDIBitmap(hdc, &ih, CBM_INIT, data, (BITMAPINFO*)&ih, DIB_RGB_COLORS);
 			if( bmp == NULL ) {
 				fprintf(stderr, "Oops. failed to create bitmap.");
 				return -1;
 			}
+			// 描画先に指定。この関数何にでも使えちゃうので型安全性という概念はどこにもないのでは
 			if( SelectObject(hdc, bmp) == 0 ) {
 				fprintf(stderr, "Oops. failed to select object.");
 				return -1;
@@ -46,6 +55,7 @@ int main(int argc, char** argv)
 		}
 		HFONT font;
 		{
+			// サンプルからとってきてフォントを作成
 			LOGFONT logfont;
 			memset (&logfont, 0, sizeof (logfont));
 
@@ -74,22 +84,30 @@ int main(int argc, char** argv)
 				return -1;
 			}
 		}
+		// 背景色を指定してるけど、あとでトランスパレントを指定してるので多分意味ない
 		if( SetBkColor(hdc, RGB(0,255,255)) == CLR_INVALID ) {
 			fprintf(stderr, "Oops. failed to set text color.");
 			return -1;
 		}
-		if( SetTextColor(hdc, RGB(255,0,0)) == CLR_INVALID ) {
+		// XXX: テキストの色を指定してるんだけど反映されないです先生
+		if( SetTextColor(hdc, RGB(255,255,0)) == CLR_INVALID ) {
 			fprintf(stderr, "Oops. failed to set text color.");
 			return -1;
 		}
+		// フォントの文字のある所以外に色を塗るかどうか指定。OPAQUEってやると矩形に色が塗られる（らしい）
+		// ここでは透明なので、元の色が残る。
 		if( SetBkMode(hdc,TRANSPARENT) == 0 ) {
 			fprintf(stderr, "Oops. failed to set mode.");
 			return -1;
 		}
+		// 文字出力
 		if( ExtTextOut(hdc, 0,0, 0, NULL, "test", 4, NULL) == 0 ) {
 			fprintf(stderr, "Oops. failed to set text color.");
 			return -1;
 		}
+		
+		// CreateDIBitmapすると新しいバッファが作られてそっちに描画されちゃうので、
+		// この関数でそのバッファを読み出す
 		GetDIBits(
 				hdc,           // デバイスコンテキストのハンドル
 				bmp,      // ビットマップのハンドル
@@ -106,12 +124,15 @@ int main(int argc, char** argv)
 	}
 	
 	//strideの問題があるのでコピー
+	// XXX:…なぜか上下反転してる…。
 	unsigned char* cdata = malloc(width*height*4);
 	for(int y=0;y<height;++y){
 		for(int x=0;x<width;++x){
 			((unsigned int*)cdata)[(height-y-1)*width + x] = *((unsigned int*)&data[(y*width+x)*3]) & 0xffffff;
 		}
 	}
+	
+	// cairoを使ってpngにコピー
 	cairo_surface_t* surf = cairo_image_surface_create_for_data(cdata, CAIRO_FORMAT_RGB24, width, height, width*4);
 	if(cairo_surface_status(surf) != CAIRO_STATUS_SUCCESS){
 		fprintf(stderr, "Oops. Invalid status: %s", cairo_status_to_string(cairo_surface_status(surf)));
