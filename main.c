b64161ea1a9c371a8dd757dcf811cb390cb2dfaa
psi <ledyba@users.sourceforge.jp>
2013-01-29 10:32:27 +0900

    色とかも反映できた。面倒くさいですねぇ。

--- main.c
+++ main.c
@@ -7,22 +7,18 @@ int main(int argc, char** argv)
 {
 	const int width = 256;
 	const int height = 256;
-	unsigned char* data = malloc(width*height*3);
-	memset(data, 0xff, width*height*3);
-	if( data == NULL ){
-		fprintf(stderr, "Oops. failed to create buffer.");
-		return -1;
-	}
+	unsigned char* data;
+	unsigned char* cdata = malloc(width*height*4);
 	{
 		// cairoにおけるcairo_t的な存在。これに対して描画コマンドを発行する
+		// この時、デフォルトの画像フォーマットは1と0の二値画像になってしまうっぽい（？）
 		HDC hdc = CreateCompatibleDC(NULL);
 		if( hdc == NULL ) {
 			fprintf(stderr, "Oops. failed to create compatible dc.");
 			return -1;
 		}
-		// cairoにおけるcairo_surface_t的な存在。
-		HBITMAP bmp;
 		// .bmpのヘッダの構造体。これを書かないとRGBデータを殆ど扱えない（驚愕）
+		// これで扱うフォーマットを指定。今回はRGB24ビット。
 		BITMAPINFOHEADER ih;
 		{
 			memset(&ih, 0, sizeof(BITMAPINFOHEADER));
@@ -37,13 +33,17 @@ int main(int argc, char** argv)
 			ih.biYPelsPerMeter = 0;
 			ih.biClrUsed = 0;
 			ih.biClrImportant = 0;
-			// 前後のihの違いがよくわかんないんだけど、
-			// 前者は初期データのフォーマットを、
-			// 後者は作られるビットマップのフォーマットを
-			// それぞれ示してるみたい。で、一緒のフォーマット（RGB24）なので同じ内容になる。
-			// dataに初期データを入れてCBM_INITを指定するとそれが初期の絵になる。
-			// ここではmemsetで0xffで指定したので真っ白。
-			bmp = CreateDIBitmap(hdc, &ih, CBM_INIT, data, (BITMAPINFO*)&ih, DIB_RGB_COLORS);
+		}
+		// cairoにおけるcairo_surface_t的な存在。
+		HBITMAP bmp;
+		{
+			// bppやサイズを利用してビットマップを作成し、その出力先の生メモリも取得できる。
+			// 最初CreateDIBitmap使ってたんだけど、これだとbppとかのフォーマットがCreateCompatibleDCで
+			// 作ったDCに合わされて（？）1/0の二値画像になってしまって使い物にならなかった
+			// 参考：http://eseken.dtiblog.com/blog-entry-74.html
+			// "そもそも CreateCompatibleDC をして得られた device context は全く使えもしない 
+			// 1 pixel * 1 pixel の白黒の bitmap などを持っている"
+			bmp = CreateDIBSection(NULL, (BITMAPINFO*)&ih, DIB_RGB_COLORS, (void **)(&data), NULL, 0);
 			if( bmp == NULL ) {
 				fprintf(stderr, "Oops. failed to create bitmap.");
 				return -1;
@@ -86,7 +86,7 @@ int main(int argc, char** argv)
 				return -1;
 			}
 		}
-		// XXX: テキストの色を指定してるんだけど反映されないです先生
+		// テキストの色を設定
 		if( SetTextColor(hdc, RGB(255,255,0)) == CLR_INVALID ) {
 			fprintf(stderr, "Oops. failed to set text color.");
 			return -1;
@@ -102,41 +102,32 @@ int main(int argc, char** argv)
 			fprintf(stderr, "Oops. failed to set mode.");
 			return -1;
 		}
-		// 色を取得して出力。RGB(255,255,0)の結果は？
-		fprintf(stdout, "color: %08x\n", GetTextColor(hdc));
+		// 文字出力のためのメモリサイズはこうすると取得できるよ
+		SIZE size;
+		GetTextExtentPoint32(hdc, "test", 4, &size);
+		fprintf(stdout, "size: %dx%d\n",size.cx, size.cy);
 		// 文字出力
 		if( ExtTextOut(hdc, 0,0, 0, NULL, "test", 4, NULL) == 0 ) {
 			fprintf(stderr, "Oops. failed to set text color.");
 			return -1;
 		}
 		
-		// CreateDIBitmapすると新しいバッファが作られてそっちに描画される。
-		// cairo_image_surface_create_for_dataみたいに既存のデータに描画されるわけではない。
-		// この関数でそのバッファを読み戻す。
-		GetDIBits(
-				hdc,           // デバイスコンテキストのハンドル
-				bmp,      // ビットマップのハンドル
-				0,   // 取得対象の最初の走査行
-				256,   // 取得対象の走査行の数
-				data,    // ビットマップのビットからなる配列
-				(BITMAPINFO*)&ih, // ビットマップデータのバッファ
-				DIB_RGB_COLORS// RGB とパレットインデックスのどちらか
-				);
-
+		{
+			//CreateDIBSectionで取得できたchar*は、そのまま描画した後にコピーしてもよいらしい。
+			
+			// cairoのRGBはあくまでピクセルごとに４バイト(1バイト余る)なのでそれに合わせる
+			// 上下反転してるのはbitmapの仕様
+			// http://www.umekkii.jp/data/computer/file_format/bitmap.cgi
+			// "通常画像データは左下から右上に記録されています。つまり、上下が反転しています。"
+			for(int y=0;y<height;++y){
+				for(int x=0;x<width;++x){
+					((unsigned int*)cdata)[(height-y-1)*width + x] = *((unsigned int*)&data[(y*width+x)*3]) & 0xffffff;
+				}
+			}
+		}
 		DeleteObject(bmp);
 		DeleteObject(font);
 		DeleteDC(hdc);
-	}
-	
-	// cairoのRGBはあくまでピクセルごとに４バイト(1バイト余る)なのでそれに合わせる
-	// 上下反転してるのはbitmapの仕様
-	// http://www.umekkii.jp/data/computer/file_format/bitmap.cgi
-	// "通常画像データは左下から右上に記録されています。つまり、上下が反転しています。"
-	unsigned char* cdata = malloc(width*height*4);
-	for(int y=0;y<height;++y){
-		for(int x=0;x<width;++x){
-			((unsigned int*)cdata)[(height-y-1)*width + x] = *((unsigned int*)&data[(y*width+x)*3]) & 0xffffff;
-		}
 	}
 	
 	// cairoを使ってpngに保存
@@ -147,7 +138,6 @@ int main(int argc, char** argv)
 	}
 	cairo_surface_write_to_png(surf, "test.png");
 	cairo_surface_destroy(surf);
-	free(data);
 	free(cdata);
 	return 0;
 }
